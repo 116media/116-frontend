420352d4469e655a6f746e240291ce50398a189e
coolbeatz71 <sigmacool@gmail.com>
2026-05-09 19:27:53 +0200

    chore(next-config): remove trailing commas from remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,10 @@ const nextConfig: NextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "i.pravatar.cc",
-            },
-        ],
-    },
+                hostname: "i.pravatar.cc"
+            }
+        ]
+    }
 };
 
 export default nextConfig;
