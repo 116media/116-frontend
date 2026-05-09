de5842474d2ae52f277a45cfba1e7fb54fb97f58
coolbeatz71 <sigmacool@gmail.com>
2026-05-09 16:03:44 +0200

    feat(ui): add dropdown menu barrel export

---
+++ src/shared/presentation/components/ui/DropdownMenu/index.ts
@@ -0,0 +1,8 @@
+export {
+    DropdownMenu,
+    DropdownMenuGroup,
+    DropdownMenuPortal,
+    DropdownMenuTrigger
+} from "./DropdownMenu";
+export { DropdownMenuContent } from "./DropdownMenuContent";
+export { DropdownMenuItem } from "./DropdownMenuItem";
