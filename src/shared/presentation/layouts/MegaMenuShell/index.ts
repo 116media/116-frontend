3b44fb0411689976a1f40a0b326612ca5dcc196f
coolbeatz71 <sigmacool@gmail.com>
2026-05-16 14:48:09 +0200

    chore(mega-menu-shell): add barrel export

---
+++ src/shared/presentation/layouts/MegaMenuShell/index.ts
@@ -0,0 +1,6 @@
+export type { MegaMenuShellProps, MegaMenuTagItem } from "./MegaMenuShell";
+export { MegaMenuShell } from "./MegaMenuShell";
+export type { MegaMenuShellCardsProps } from "./MegaMenuShellCards";
+export { MegaMenuShellCards } from "./MegaMenuShellCards";
+export type { MegaMenuShellCategoriesProps } from "./MegaMenuShellCategories";
+export { MegaMenuShellCategories } from "./MegaMenuShellCategories";
