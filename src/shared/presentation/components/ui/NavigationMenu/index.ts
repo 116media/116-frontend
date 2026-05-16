472a1cdc42981390860d01dc9071b24fba1deda7
coolbeatz71 <sigmacool@gmail.com>
2026-05-16 13:39:29 +0200

    chore(navigation-menu): add barrel export

---
+++ src/shared/presentation/components/ui/NavigationMenu/index.ts
@@ -0,0 +1,7 @@
+export { NavigationMenu } from "./NavigationMenu";
+export { NavigationMenuContent } from "./NavigationMenuContent";
+export { NavigationMenuItem } from "./NavigationMenuItem";
+export { NavigationMenuLink } from "./NavigationMenuLink";
+export { NavigationMenuList } from "./NavigationMenuList";
+export { NavigationMenuTrigger } from "./NavigationMenuTrigger";
+export { NavigationMenuViewport } from "./NavigationMenuViewport";
