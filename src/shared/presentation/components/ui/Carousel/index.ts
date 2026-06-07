bd99251bf04c43fbecf046e48059f6351bb9a776
coolbeatz71 <sigmacool@gmail.com>
2026-06-07 18:03:15 +0200

    feat(carousel): add barrel export

---
+++ src/shared/presentation/components/ui/Carousel/index.ts
@@ -0,0 +1,4 @@
+export type { CarouselProps } from "./Carousel";
+export { Carousel, useCarousel } from "./Carousel";
+export { CarouselContent } from "./CarouselContent";
+export { CarouselItem } from "./CarouselItem";
