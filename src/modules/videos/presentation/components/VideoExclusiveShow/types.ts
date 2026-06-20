e6e97cb1728288452a31b50511b43609f1822f26
coolbeatz71 <sigmacool@gmail.com>
2026-06-20 15:05:11 +0200

    refactor(video-exclusive-show): add poster and episodes prop types

--- src/modules/videos/presentation/components/VideoExclusiveShow/types.ts
+++ src/modules/videos/presentation/components/VideoExclusiveShow/types.ts
@@ -1,4 +1,5 @@
 import type { IVideoExclusiveShowEntity } from "@/modules/videos/domain/entities/IVideoExclusiveShowEntity";
+import type { IVideoSummaryEntity } from "@/modules/videos/domain/entities/IVideoSummaryEntity";
 
 /**
  * VideoExclusiveShowViewProps
@@ -10,4 +11,28 @@ import type { IVideoExclusiveShowEntity } from "@/modules/videos/domain/entities
  */
 export interface VideoExclusiveShowViewProps {
     category: IVideoExclusiveShowEntity;
+}
+
+/**
+ * VideoExclusiveShowPosterProps
+ *
+ * @description
+ * Props for the poster panel (left side) of the exclusive show section.
+ *
+ * @property {IVideoExclusiveShowEntity} category - The exclusive category (show)
+ */
+export interface VideoExclusiveShowPosterProps {
+    category: IVideoExclusiveShowEntity;
+}
+
+/**
+ * VideoExclusiveShowEpisodesProps
+ *
+ * @description
+ * Props for the episodes panel (right side) of the exclusive show section.
+ *
+ * @property {IVideoSummaryEntity[]} episodes - The show's episodes
+ */
+export interface VideoExclusiveShowEpisodesProps {
+    episodes: IVideoSummaryEntity[];
 }
