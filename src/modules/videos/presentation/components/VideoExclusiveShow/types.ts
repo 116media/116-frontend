2ff66c821b822e4712945e6a7eeb7c6a892145a6
coolbeatz71 <sigmacool@gmail.com>
2026-06-20 16:29:13 +0200

    refactor(video-exclusive-show): drop relocated panel prop types

--- src/modules/videos/presentation/components/VideoExclusiveShow/types.ts
+++ src/modules/videos/presentation/components/VideoExclusiveShow/types.ts
@@ -1,5 +1,4 @@
 import type { IVideoExclusiveShowEntity } from "@/modules/videos/domain/entities/IVideoExclusiveShowEntity";
-import type { IVideoSummaryEntity } from "@/modules/videos/domain/entities/IVideoSummaryEntity";
 
 /**
  * VideoExclusiveShowViewProps
@@ -11,28 +10,4 @@ import type { IVideoSummaryEntity } from "@/modules/videos/domain/entities/IVide
  */
 export interface VideoExclusiveShowViewProps {
     category: IVideoExclusiveShowEntity;
-}
-
-/**
- * VideoExclusiveShowPosterProps
- *
- * @description
- * Props for the poster panel (left side) of the exclusive show section.
- *
- * @property {IVideoExclusiveShowEntity} category - The exclusive category (show)
- */
-export interface VideoExclusiveShowPosterProps {
-    category: IVideoExclusiveShowEntity;
-}
-
-/**
- * VideoExclusiveShowEpisodesProps
- *
- * @description
- * Props for the episodes panel (right side) of the exclusive show section.
- *
- * @property {IVideoSummaryEntity[]} episodes - The show's episodes
- */
-export interface VideoExclusiveShowEpisodesProps {
-    episodes: IVideoSummaryEntity[];
 }
