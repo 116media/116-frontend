ba404dfc1337ef448dd36571fccfe3fb9223dae1
coolbeatz71 <sigmacool@gmail.com>
2026-06-20 10:28:23 +0200

    feat(videos): register exclusive show use case in container

--- src/modules/videos/infrastructure/dependencies/videos.dependencies.ts
+++ src/modules/videos/infrastructure/dependencies/videos.dependencies.ts
@@ -2,6 +2,7 @@ import type { AwilixContainer } from "awilix";
 import { asClass } from "awilix";
 import { GetPromotedVideosUseCase } from "@/modules/videos/application/usecases/getpromotedvideos.usecase";
 import { GetVideoCategoriesUseCase } from "@/modules/videos/application/usecases/getvideocategories.usecase";
+import { GetVideoExclusiveShowUseCase } from "@/modules/videos/application/usecases/getvideoexclusiveshow.usecase";
 import { GetVideoPopularTagsUseCase } from "@/modules/videos/application/usecases/getvideopopulartags.usecase";
 import { VideosRepositoryImpl } from "@/modules/videos/infrastructure/repositories/videos.repository.impl";
 
@@ -22,6 +23,7 @@ export function registerVideosDependencies(container: AwilixContainer): void {
         // Queries
         getPromotedVideosUseCase: asClass(GetPromotedVideosUseCase).transient(),
         getVideoCategoriesUseCase: asClass(GetVideoCategoriesUseCase).transient(),
-        getVideoPopularTagsUseCase: asClass(GetVideoPopularTagsUseCase).transient()
+        getVideoPopularTagsUseCase: asClass(GetVideoPopularTagsUseCase).transient(),
+        getVideoExclusiveShowUseCase: asClass(GetVideoExclusiveShowUseCase).transient()
     });
 }
