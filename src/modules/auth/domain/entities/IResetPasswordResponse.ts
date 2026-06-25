e1d50c3e5435ce5da66ee818c50dd8844c1abc3d
coolbeatz71 <sigmacool@gmail.com>
2026-06-25 02:41:08 +0200

    feat(auth): add reset-password response entity

---
+++ src/modules/auth/domain/entities/IResetPasswordResponse.ts
@@ -0,0 +1,10 @@
+import type { IActionResponse } from "@/shared/domain/types/action.response";
+
+/**
+ * IResetPasswordResponse
+ *
+ * @description Response from completing a password reset.
+ * @interface IResetPasswordResponse
+ * @extends {IActionResponse}
+ */
+export interface IResetPasswordResponse extends IActionResponse {}
