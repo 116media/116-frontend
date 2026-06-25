f99461ce5453439aaedc03765c616563941792d3
coolbeatz71 <sigmacool@gmail.com>
2026-06-25 03:50:12 +0200

    feat(auth): add sign-out response entity

---
+++ src/modules/auth/domain/entities/ISignOutResponse.ts
@@ -0,0 +1,10 @@
+import type { IActionResponse } from "@/shared/domain/types/action.response";
+
+/**
+ * ISignOutResponse
+ *
+ * @description Response from signing out the current session.
+ * @interface ISignOutResponse
+ * @extends {IActionResponse}
+ */
+export interface ISignOutResponse extends IActionResponse {}
