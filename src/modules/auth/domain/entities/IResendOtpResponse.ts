b4b3aea591ce0e5e2054eb77c0759ee930ea0aa2
coolbeatz71 <sigmacool@gmail.com>
2026-06-25 02:24:47 +0200

    feat(auth): add resend-otp response entity

---
+++ src/modules/auth/domain/entities/IResendOtpResponse.ts
@@ -0,0 +1,10 @@
+import type { IActionResponse } from "@/shared/domain/types/action.response";
+
+/**
+ * IResendOtpResponse
+ *
+ * @description Response from resending an OTP.
+ * @interface IResendOtpResponse
+ * @extends {IActionResponse}
+ */
+export interface IResendOtpResponse extends IActionResponse {}
