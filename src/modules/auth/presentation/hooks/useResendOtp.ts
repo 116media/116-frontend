b45cd2a5429675851fa94f4f45e68f3f45f6cdb5
coolbeatz71 <sigmacool@gmail.com>
2026-06-28 17:18:44 +0200

    chore(auth): update authKeys import path

--- src/modules/auth/presentation/hooks/useResendOtp.ts
+++ src/modules/auth/presentation/hooks/useResendOtp.ts
@@ -3,7 +3,7 @@
 import { useMutation } from "@tanstack/react-query";
 
 import type { IResendOtpResponse } from "@/modules/auth/domain/entities/IResendOtpResponse";
-import { authKeys } from "@/modules/auth/presentation/context/authKeys";
+import { authKeys } from "@/modules/auth/presentation/constants/authKeys";
 import type { IResendOtpCredentials } from "@/modules/auth/presentation/model/IResendOtpCredentials";
 import type { Failure } from "@/shared/domain/failures/failure";
 import container from "@/shared/infrastructure/service.locator";
