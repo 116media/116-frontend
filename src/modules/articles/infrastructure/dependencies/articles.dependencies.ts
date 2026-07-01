ffd09f6a64f78e8714de7c3f139cfa4a1f135320
coolbeatz71 <sigmacool@gmail.com>
2026-07-01 11:57:34 +0200

    feat(articles): register detail use cases in container

--- src/modules/articles/infrastructure/dependencies/articles.dependencies.ts
+++ src/modules/articles/infrastructure/dependencies/articles.dependencies.ts
@@ -1,10 +1,14 @@
 import type { AwilixContainer } from "awilix";
 import { asClass } from "awilix";
+import { AddArticleCommentUseCase } from "@/modules/articles/application/usecases/addarticlecomment.usecase";
 import { BookmarkArticleUseCase } from "@/modules/articles/application/usecases/bookmarkarticle.usecase";
 import { GetAllTagsUseCase } from "@/modules/articles/application/usecases/getalltags.usecase";
+import { GetArticleBySlugUseCase } from "@/modules/articles/application/usecases/getarticlebyslug.usecase";
 import { GetArticleCategoriesUseCase } from "@/modules/articles/application/usecases/getarticlecategories.usecase";
+import { GetArticleCommentsUseCase } from "@/modules/articles/application/usecases/getarticlecomments.usecase";
 import { GetArticlePopularTagsUseCase } from "@/modules/articles/application/usecases/getarticlepopulartags.usecase";
 import { GetArticlePromotionFeedUseCase } from "@/modules/articles/application/usecases/getarticlepromotionfeed.usecase";
+import { GetPopularArticlesUseCase } from "@/modules/articles/application/usecases/getpopulararticles.usecase";
 import { GetPromotedArticlesUseCase } from "@/modules/articles/application/usecases/getpromotedarticles.usecase";
 import { GetPublishedArticlesUseCase } from "@/modules/articles/application/usecases/getpublishedarticles.usecase";
 import { LikeArticleUseCase } from "@/modules/articles/application/usecases/likearticle.usecase";
@@ -34,12 +38,16 @@ export function registerArticlesDependencies(container: AwilixContainer): void {
         getArticlePromotionFeedUseCase: asClass(GetArticlePromotionFeedUseCase).transient(),
         getPublishedArticlesUseCase: asClass(GetPublishedArticlesUseCase).transient(),
         getAllTagsUseCase: asClass(GetAllTagsUseCase).transient(),
+        getArticleBySlugUseCase: asClass(GetArticleBySlugUseCase).transient(),
+        getArticleCommentsUseCase: asClass(GetArticleCommentsUseCase).transient(),
+        getPopularArticlesUseCase: asClass(GetPopularArticlesUseCase).transient(),
 
         // Interactions
         likeArticleUseCase: asClass(LikeArticleUseCase).transient(),
         unlikeArticleUseCase: asClass(UnlikeArticleUseCase).transient(),
         bookmarkArticleUseCase: asClass(BookmarkArticleUseCase).transient(),
         unbookmarkArticleUseCase: asClass(UnbookmarkArticleUseCase).transient(),
-        shareArticleUseCase: asClass(ShareArticleUseCase).transient()
+        shareArticleUseCase: asClass(ShareArticleUseCase).transient(),
+        addArticleCommentUseCase: asClass(AddArticleCommentUseCase).transient()
     });
 }
