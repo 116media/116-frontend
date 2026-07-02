f138b9e9ed504d076655ade8ee9b450642a21900
coolbeatz71 <sigmacool@gmail.com>
2026-07-02 15:45:36 +0200

    feat(videos): register detail use cases in container

--- src/modules/videos/infrastructure/dependencies/videos.dependencies.ts
+++ src/modules/videos/infrastructure/dependencies/videos.dependencies.ts
@@ -1,10 +1,19 @@
 import type { AwilixContainer } from "awilix";
 import { asClass } from "awilix";
+import { AddVideoToPlaylistUseCase } from "@/modules/videos/application/usecases/addvideotoplaylist.usecase";
+import { CreatePlaylistUseCase } from "@/modules/videos/application/usecases/createplaylist.usecase";
+import { GetMyPlaylistsUseCase } from "@/modules/videos/application/usecases/getmyplaylists.usecase";
 import { GetPromotedVideosUseCase } from "@/modules/videos/application/usecases/getpromotedvideos.usecase";
+import { GetPublishedVideosUseCase } from "@/modules/videos/application/usecases/getpublishedvideos.usecase";
 import { GetShowsUseCase } from "@/modules/videos/application/usecases/getshows.usecase";
+import { GetVideoBySlugUseCase } from "@/modules/videos/application/usecases/getvideobyslug.usecase";
 import { GetVideoCategoriesUseCase } from "@/modules/videos/application/usecases/getvideocategories.usecase";
 import { GetVideoExclusiveShowUseCase } from "@/modules/videos/application/usecases/getvideoexclusiveshow.usecase";
+import { GetVideoLyricsUseCase } from "@/modules/videos/application/usecases/getvideolyrics.usecase";
 import { GetVideoPopularTagsUseCase } from "@/modules/videos/application/usecases/getvideopopulartags.usecase";
+import { GetYoutubeVideoStatsUseCase } from "@/modules/videos/application/usecases/getyoutubevideostats.usecase";
+import { RateVideoUseCase } from "@/modules/videos/application/usecases/ratevideo.usecase";
+import { ShareVideoUseCase } from "@/modules/videos/application/usecases/sharevideo.usecase";
 import { VideosRepositoryImpl } from "@/modules/videos/infrastructure/repositories/videos.repository.impl";
 
 /**
@@ -26,6 +35,17 @@ export function registerVideosDependencies(container: AwilixContainer): void {
         getVideoCategoriesUseCase: asClass(GetVideoCategoriesUseCase).transient(),
         getShowsUseCase: asClass(GetShowsUseCase).transient(),
         getVideoPopularTagsUseCase: asClass(GetVideoPopularTagsUseCase).transient(),
-        getVideoExclusiveShowUseCase: asClass(GetVideoExclusiveShowUseCase).transient()
+        getVideoExclusiveShowUseCase: asClass(GetVideoExclusiveShowUseCase).transient(),
+        getVideoBySlugUseCase: asClass(GetVideoBySlugUseCase).transient(),
+        getPublishedVideosUseCase: asClass(GetPublishedVideosUseCase).transient(),
+        getVideoLyricsUseCase: asClass(GetVideoLyricsUseCase).transient(),
+        getMyPlaylistsUseCase: asClass(GetMyPlaylistsUseCase).transient(),
+        getYoutubeVideoStatsUseCase: asClass(GetYoutubeVideoStatsUseCase).transient(),
+
+        // Interactions
+        rateVideoUseCase: asClass(RateVideoUseCase).transient(),
+        shareVideoUseCase: asClass(ShareVideoUseCase).transient(),
+        createPlaylistUseCase: asClass(CreatePlaylistUseCase).transient(),
+        addVideoToPlaylistUseCase: asClass(AddVideoToPlaylistUseCase).transient()
     });
 }
