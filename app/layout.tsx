e3bd64164727de44cf6dd108cc13e08c24835afb
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-07 03:34:40 +0200

    refactor(app): update language import path

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import { Toaster } from "@/shared/presentation/components/ui/Toaster";
 import { I18nProvider } from "@/shared/presentation/i18n/I18nProvider";
 import { QueryProvider } from "@/shared/presentation/providers/QueryProvider";
 import { ThemeProvider } from "@/shared/presentation/providers/ThemeProvider";
-import { getServerLanguage } from "@/shared/presentation/utils/getServerLanguage";
+import { getServerLanguage } from "@/shared/presentation/utils/language/language.server.utils";
 import "./globals.css";
 
 const outfit = Outfit({
