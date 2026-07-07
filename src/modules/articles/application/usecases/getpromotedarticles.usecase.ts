21879491d216841975036e617656464bfb2c1a86
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-07 04:56:42 +0200

    docs(articles): tighten GetPromotedArticlesUseCase jsdoc to 3 lines

--- src/modules/articles/application/usecases/getpromotedarticles.usecase.ts
+++ src/modules/articles/application/usecases/getpromotedarticles.usecase.ts
@@ -23,7 +23,7 @@ export class GetPromotedArticlesUseCase implements IGetPromotedArticlesUseCase {
     private readonly articlesRepository: IArticlesRepositoryPort;
 
     /**
-     * @param {IArticlesRepositoryPort} articlesRepository - Repository for articles operations (injected)
+     * @param articlesRepository - Repository for articles operations (injected)
      */
     constructor({ articlesRepository }: { articlesRepository: IArticlesRepositoryPort }) {
         this.articlesRepository = articlesRepository;
