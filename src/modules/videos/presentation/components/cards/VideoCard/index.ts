e85efdbb2b3756edd857608d69002731b9db656d
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-08 10:20:39 +0200

    refactor(videos): move VideoCard barrel to cards

---
+++ src/modules/videos/presentation/components/cards/VideoCard/index.ts
@@ -0,0 +1,12 @@
+import { Horizontal } from "./VideoCard.Horizontal";
+import { Vertical } from "./VideoCard.Vertical";
+
+/**
+ * VideoCard
+ *
+ * @description
+ * Compound component for video (episode) cards: `.Horizontal` (bordered row)
+ * and `.Vertical` (poster-style grid card). Each variant renders one video in
+ * a different layout; members are documented at their own files.
+ */
+export const VideoCard = { Horizontal, Vertical } as const;
