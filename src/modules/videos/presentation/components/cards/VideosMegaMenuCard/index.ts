b9f2583284c24cf680677afec3e2dab0f7e3bc68
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-08 10:58:01 +0200

    refactor(videos): move MegaMenuCard barrel to cards

---
+++ src/modules/videos/presentation/components/cards/VideosMegaMenuCard/index.ts
@@ -0,0 +1,12 @@
+import { Compact } from "./VideosMegaMenuCard.Compact";
+import { FeaturedFullBleed } from "./VideosMegaMenuCard.FeaturedFullBleed";
+import { FeaturedSpotlight } from "./VideosMegaMenuCard.FeaturedSpotlight";
+
+/**
+ * VideosMegaMenuCard
+ *
+ * @description
+ * Compound card family for the mega menu "À la une" column: `.FeaturedFullBleed`,
+ * `.FeaturedSpotlight`, and `.Compact`. Members are documented at their own files.
+ */
+export const VideosMegaMenuCard = { FeaturedFullBleed, FeaturedSpotlight, Compact };
