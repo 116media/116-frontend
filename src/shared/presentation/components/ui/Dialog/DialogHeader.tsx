45d23cbdbe15ee057972a425c7cdc8e3bbd32127
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-09 08:31:37 +0200

    refactor(dialog): update cn import path

--- src/shared/presentation/components/ui/Dialog/DialogHeader.tsx
+++ src/shared/presentation/components/ui/Dialog/DialogHeader.tsx
@@ -1,6 +1,6 @@
 import type { HTMLAttributes } from "react";
 
-import { cn } from "@/shared/presentation/utils/cn";
+import { cn } from "@/shared/presentation/utils/cn/cn.utils";
 
 /**
  * DialogHeader
