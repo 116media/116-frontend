f5bb3831bde6a19cb4c35ed8cb8d37cc0942747b
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-09 07:25:56 +0200

    refactor(carousel): update useCarousel import path

--- src/shared/presentation/components/ui/Carousel/index.ts
+++ src/shared/presentation/components/ui/Carousel/index.ts
@@ -1,4 +1,5 @@
 export type { CarouselProps } from "./Carousel";
-export { Carousel, useCarousel } from "./Carousel";
+export { Carousel } from "./Carousel";
 export { CarouselContent } from "./CarouselContent";
 export { CarouselItem } from "./CarouselItem";
+export { useCarousel } from "./useCarousel";
