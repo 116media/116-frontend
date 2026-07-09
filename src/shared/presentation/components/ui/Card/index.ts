7e4554cf67089c3ae13ee14e2389ef93024cecdf
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-09 07:01:38 +0200

    chore(card): drop deleted part from barrel

--- src/shared/presentation/components/ui/Card/index.ts
+++ src/shared/presentation/components/ui/Card/index.ts
@@ -1,5 +1,2 @@
 export type { CardProps } from "./Card";
 export { Card, cardVariants } from "./Card";
-export { CardContent } from "./CardContent";
-export { CardFooter } from "./CardFooter";
-export { CardHeader } from "./CardHeader";
