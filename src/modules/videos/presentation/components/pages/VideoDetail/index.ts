027fd2bf430c89d8a111a65862a97aed0f9042c7
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-10 09:31:16 +0200

    chore(videos): add VideoDetail barrel

---
+++ src/modules/videos/presentation/components/pages/VideoDetail/index.ts
@@ -0,0 +1,4 @@
+export { VideoDetail, type VideoDetailProps } from "./VideoDetail";
+export { VideoDetailError } from "./VideoDetail.Error";
+export { VideoDetailLoading } from "./VideoDetail.Loading";
+export { VideoDetailNotFound } from "./VideoDetail.NotFound";
