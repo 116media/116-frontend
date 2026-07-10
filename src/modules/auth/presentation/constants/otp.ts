5b631fec5bc1a1d6ae32d131272f85aefaf21ba2
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-10 06:57:27 +0200

    refactor(auth): add OTP_LENGTH constant

---
+++ src/modules/auth/presentation/constants/otp.ts
@@ -0,0 +1,9 @@
+/**
+ * Length of the one-time code the backend issues.
+ */
+export const OTP_LENGTH = 6;
+
+/**
+ * Cooldown before another OTP resend is allowed, in seconds.
+ */
+export const RESEND_COOLDOWN_SECONDS = 60;
