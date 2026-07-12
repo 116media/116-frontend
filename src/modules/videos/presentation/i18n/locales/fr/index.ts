db94d128d3a946f20b979ce8c76d831bf22cfed4
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-12 05:02:21 +0200

    chore(i18n): register french browse namespace

--- src/modules/videos/presentation/i18n/locales/fr/index.ts
+++ src/modules/videos/presentation/i18n/locales/fr/index.ts
@@ -1,3 +1,4 @@
+import { browse } from "@/modules/videos/presentation/i18n/locales/fr/browse";
 import { exclusiveShow } from "@/modules/videos/presentation/i18n/locales/fr/exclusiveshow";
 import { home } from "@/modules/videos/presentation/i18n/locales/fr/home";
 import { videoDetail } from "@/modules/videos/presentation/i18n/locales/fr/video-detail";
@@ -12,6 +13,7 @@ import { videoDetail } from "@/modules/videos/presentation/i18n/locales/fr/video
  */
 export const fr = {
     home,
+    browse,
     exclusiveShow,
     ...videoDetail
 } as const;
