8289db1487c80a5d4fd0c52f8f894e3b292233d7
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-14 12:38:29 +0200

    style(exclusive-show): sort split type import

--- src/modules/videos/presentation/components/sections/VideoExclusiveShow/VideoExclusiveShow.Split.tsx
+++ src/modules/videos/presentation/components/sections/VideoExclusiveShow/VideoExclusiveShow.Split.tsx
@@ -1,3 +1,4 @@
+import type { VideoExclusiveShowViewProps } from "./types";
 import { VideoExclusiveShowEpisodes } from "./VideoExclusiveShow.Episodes";
 import { VideoExclusiveShowHeading } from "./VideoExclusiveShow.Heading";
 import { VideoExclusiveShowPoster } from "./VideoExclusiveShow.Poster";
@@ -5,7 +6,6 @@ import { VideoExclusiveShowRoot } from "./VideoExclusiveShow.Root";
 import { VideoExclusiveShowScrim } from "./VideoExclusiveShow.Scrim";
 import { VideoExclusiveShowTag } from "./VideoExclusiveShow.Tag";
 import { VideoExclusiveShowWatchButton } from "./VideoExclusiveShow.WatchButton";
-import type { VideoExclusiveShowViewProps } from "./types";
 
 /**
  * Split
