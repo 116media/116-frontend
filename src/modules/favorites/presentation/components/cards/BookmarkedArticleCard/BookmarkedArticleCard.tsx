3b74e7b86f5cff86ca7eb75da5f1d1f439468c2a
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-17 11:28:37 +0200

    style(favorites): highlight bookmarked article metadata

--- src/modules/favorites/presentation/components/cards/BookmarkedArticleCard/BookmarkedArticleCard.tsx
+++ src/modules/favorites/presentation/components/cards/BookmarkedArticleCard/BookmarkedArticleCard.tsx
@@ -45,6 +45,7 @@ export function BookmarkedArticleCard({ bookmark }: BookmarkedArticleCardProps) 
                 <FavoriteCard.Meta
                     dateKind="bookmarked"
                     date={bookmark.bookmarkedAt}
+                    className="-mx-3 -mb-3 mt-1 flex flex-col gap-3 rounded-lg bg-accent/50 p-3"
                 />
             </FavoriteArticleContent>
         </FavoriteCard>
